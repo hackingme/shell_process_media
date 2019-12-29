3b155745dd299b570d1919af176908fac92491c2
hackingme <hackingme@hackingme.hackingme>
2019-12-29 13:52:38 -0800

    Handle special characters in the file names

--- process_all_media.sh
+++ process_all_media.sh
@@ -4,10 +4,11 @@ trap "echo Fail unexpectedly on line \$LINENO! > &2" ERR
 for t in JPG jpg mp4 MP4 MOV; do
   for f in *.$t; do 
     if test -f "$f"; then
-      folder=$(date -f'%F %T %z' -j "$(mdls -raw -name kMDItemContentCreationDate $f)" +%Y-%m-%d)
+      date=$(mdls -raw -name kMDItemContentCreationDate "$f")
+      folder=$(date -f'%F %T %z' -j "$date" +%Y-%m-%d)
       mkdir -p $folder
-      echo $f $folder
-      mv $f $folder
+      echo "$f" $folder
+      mv "$f" $folder
     fi
   done
 done

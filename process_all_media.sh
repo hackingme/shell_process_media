3e5bc764e26a97c4b4276ca468468163d9f82ed6
hackingme <hackingme@hackingme.hackingme>
2021-02-14 14:17:55 -0800

    pick valid date from either kMDItemContentCreationDate or kMDItemFSContentChangeDate

--- process_all_media.sh
+++ process_all_media.sh
@@ -4,7 +4,9 @@ trap "echo Fail unexpectedly on line \$LINENO! > &2" ERR
 for t in JPG jpg mp4 MP4 MOV; do
   for f in *.$t; do 
     if test -f "$f"; then
-      date=$(mdls -raw -name kMDItemContentCreationDate "$f")
+      date1=$(mdls -raw -name kMDItemContentCreationDate "$f")
+      date2=$(mdls -raw -name kMDItemFSContentChangeDate "$f")
+      date=$([[ "$date1" < "$date2" ]] && echo "$date1" || echo "$date2")
       folder=$(date -f'%F %T %z' -j "$date" +%Y-%m-%d)
       mkdir -p $folder
       echo "$f" $folder
